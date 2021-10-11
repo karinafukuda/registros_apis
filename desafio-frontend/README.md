d9f10a0d732cf83ebecb97993adace41d5598ee6
Karina Fukuda <karina.fukuda@inmetrics.com.br>
2021-10-11 03:24:25 -0300

    identacao, barra rolagem lista, bugs e README

---
+++ desafio-frontend/README.md
@@ -0,0 +1,8 @@
+## Em processo
+---
+
+<p>Feito: Interface web, lista, forms, consulta api LIST/GET Clientes, consulta API Via CEP e formulário, com Javascript / CSS / HTML / AJAX / Bootstrap. </p>
+<br/>
+<p>* Faltou (aprendendo ainda...): Requisição POST / GET :id / DELETE ;</p>
+
+###
--- desafio-frontend/js/busca-cep.js
+++ desafio-frontend/js/busca-cep.js
@@ -1,15 +1,15 @@
 function consultaCep() {
- var cep = document.getElementById("cep").value;
- var url = "https://viacep.com.br/ws/" + cep + "/json/";
- $.ajax({
-     url: url ,
-     type:"GET",
-     success: function(response){
-         console.log(response);
-         document.getElementById("logradouro").innerHTML = response.logradouro;
-         document.getElementById("bairro").innerHTML = response.bairro;
-         document.getElementById("localidade").innerHTML = response.localidade;
-         document.getElementById("uf").innerHTML = response.uf;
-     }
- })
+  var cep = document.getElementById("cep").value;
+  var url = "https://viacep.com.br/ws/" + cep + "/json/";
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function (response) {
+      console.log(response);
+      document.getElementById("logradouro").innerHTML = response.logradouro;
+      document.getElementById("bairro").innerHTML = response.bairro;
+      document.getElementById("localidade").innerHTML = response.localidade;
+      document.getElementById("uf").innerHTML = response.uf;
+    },
+  });
 }
